b402a6df48272353bb69a683223df53524e836c2
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-11 20:17:37 -0300

    feat: multiply struct

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ContaCorrente struct {
 	titular       string
 	numeroAgencia int
@@ -8,5 +10,16 @@ type ContaCorrente struct {
 }
 
 func main() {
+	contaDoWermenson := ContaCorrente{titular: "Wermenson",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
 
+	contaDaLizz := ContaCorrente{"Lizz", 222, 111222, 1000}
+
+	var contaDaLuna *ContaCorrente
+	contaDaLuna = new(ContaCorrente)
+	contaDaLuna.titular = "Luna"
+
+	fmt.Println(contaDoWermenson)
+	fmt.Println(contaDaLizz)
+	fmt.Println(*contaDaLuna)
 }
