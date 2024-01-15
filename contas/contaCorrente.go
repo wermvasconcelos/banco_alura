c57229416ba25d75a343edda90a6432de58ea794
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-15 16:00:07 -0300

    refactor: saldo private

--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -6,33 +6,33 @@ type ContaCorrente struct {
 	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
 	if valorDeposito > 0 {
-		c.Saldo += valorDeposito
-		return "Deposito realizado com sucesso!", c.Saldo
+		c.saldo += valorDeposito
+		return "Deposito realizado com sucesso!", c.saldo
 	}
 
-	return "Valor do deposito menor que zero", c.Saldo
+	return "Valor do deposito menor que zero", c.saldo
 
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
-		c.Saldo -= valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso!"
 	}
 
-	return "Saldo insuficiente"
+	return "saldo insuficiente"
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-		c.Saldo -= valorDaTransferencia
-		contaDestino.Saldo += valorDaTransferencia
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.saldo += valorDaTransferencia
 		return true
 	}
 	return false
